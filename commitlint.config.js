d749c5ae88cfae94aaea74c3852250cd4a8ac117
KenHH-24 <1098862703@qq.com>
2024-03-10 19:43:08 +0800

    chore: modify commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test']],
-    'subject-case': [2, 'always', ['lower-case', 'pascal-case', 'kebab-case', 'sentence-case']],
+    'subject-case': [2, 'always', ['lower-case', 'pascal-case', 'kebab-case']],
   },
 }
